20cd7ab2386a491a06709490af0f4f17df02eb5c
Zmon <norkov900@gmail.com>
2020-02-21 19:52:04 +0200

    Change ui

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 import sys
+from PyQt5 import QtCore
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtGui import QImage
+from PyQt5.QtWidgets import QDesktopWidget,QApplication
 from PyQt5 import QtWidgets
 from ui import Ui_MainWindow  # импорт нашего сгенерированного файла
 
 images_type = ['.jpg', '.png', 'jpeg']
+
+# app_2 = QApplication(sys.argv)
+# resolution = QDesktopWidget().availableGeometry()
+
+# print(resolution)
 
 
 class MyWindow(QtWidgets.QMainWindow):
@@ -12,15 +21,32 @@ class MyWindow(QtWidgets.QMainWindow):
         self.ui.setupUi(self)
         self.ui.pushButton.clicked.connect(self.Buttonbegin)
         self.ui.pushButton_2.clicked.connect(self.Browsebutton)
-        self.ui.pushButton_3.clicked.connect(self.Uploadbutton)
+        self.ui.ScaleCheckBox.stateChanged.connect(self.ScaleCheck)
+        self.ui.checkBox_2.clicked.connect(self.Black_And_White)
 
-    def Uploadbutton(self):
-        pass
+    def ScaleCheck(self, state):
+
+        if state == QtCore.Qt.Checked:
+            pixmap = image.scaled(1030, 677, QtCore.Qt.KeepAspectRatio)
+            self.ui.imagelabel.setPixmap(QPixmap.fromImage(pixmap))
+        else:
+            try:
+                self.ui.imagelabel.setPixmap(QPixmap.fromImage(image))
+            except NameError as ne:
+                print(ne)
 
     def Browsebutton(self):
         filename = QtWidgets.QFileDialog.getOpenFileName(filter='Images (*.png *.xpm *.jpg *.jpeg)',
                                                          caption='Select image')
         self.ui.lineEdit.setText(filename[0])
+
+        global image
+        image = QImage(filename[0])
+
+        self.ui.imagelabel.setPixmap(QPixmap.fromImage(image))
+
+    def Black_And_White(self):
+        pass
 
     def Buttonbegin(self):
         print("Pressed")
